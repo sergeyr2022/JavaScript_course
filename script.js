00c448eeb043e03bfb230a4a20cc5e8bbfbc168a
Sergey Ryzhkov <stdexecd@gmail.com>
2022-11-17 23:26:41 +0300

    Обновил скрипт

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-console.log("hello world");
 const ourPlanetName = "Earth";
+let userCurrentTime = "13:15"; 
+let numberOfArticles = 8;
+let isCash = true;
+let userFirstName = "Сергей";
+let userLastName = "Рыжков";
+let userMiddleName = "Николаевич";
+
+console.log('Hello world!');
 console.log(ourPlanetName);
-let userTimeNow = "13:15"; 
+console.log(userCurrentTime);
+console.log(numberOfArticles);
+console.log(isCash);
+console.log(userLastName, userFirstName, userMiddleName);
