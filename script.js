35cd84b72b0fcb2e08a8063ad95ec0b564223d4d
Sergey Ryzhkov <stdexecd@gmail.com>
2022-11-22 13:47:38 +0300

    Обновление

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const ourPlanetName = "Earth";
-let userCurrentTime = "13:15"; 
-let numberOfArticles = 8;
-let isCash = true;
-let userFirstName = "Сергей";
-let userLastName = "Рыжков";
-let userMiddleName = "Николаевич";
+// const ourPlanetName = "Earth";
+// let userCurrentTime = "13:15";
+// let numberOfArticles = 8;
+// let isCash = true;
+// let userFirstName = "Сергей";
+// let userLastName = "Рыжков";
+// let userMiddleName = "Николаевич";
 
-console.log('Hello world!');
-console.log(ourPlanetName);
-console.log(userCurrentTime);
-console.log(numberOfArticles);
-console.log(isCash);
-console.log(userLastName, userFirstName, userMiddleName);
+// console.log("Hello world!");
+// console.log(ourPlanetName);
+// console.log(userCurrentTime);
+// console.log(numberOfArticles);
+// console.log(isCash);
+// console.log(userLastName, userFirstName, userMiddleName);
+// const nameUser = "Alex";
+// const age = 18;
+// const userData = `Имя пользователя: ${nameUser}, возраст: ${age}`;
+// console.log(userData);
+// const isUser = false;
+// const isVisible = true;
+// let townName;
+// console.log(townName);
+// townName = "Moscow";
+// console.log(townName);
+const userData = {
+  name: "Ivan",
+  age: 34,
+};
+console.log(typeof userData);
